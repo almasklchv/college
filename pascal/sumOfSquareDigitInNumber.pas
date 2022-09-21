561a7f550fc0342bb107711fad26e86302b874a1
Almas  Kilichov <myprojectsecret01@gmail.com>
2022-09-21 11:37:26 +0600

    docs: add sumOfSquareDigitInNumber.pas file

---
+++ pascal/sumOfSquareDigitInNumber.pas
@@ -0,0 +1,18 @@
+﻿begin
+  var number: integer;
+  var numberInStr: string;
+  var sum: integer;
+  var i: integer;
+  sum := 0;
+  
+  write('Введите число: ');
+  read(number);
+  str(number, numberInStr);
+  
+  for i := 1 to length(numberInStr) do begin
+    sum += sqr(StrToInt(copy(numberInStr, i, 1)));
+  end;
+  
+  write(sum);
+    
+end.
